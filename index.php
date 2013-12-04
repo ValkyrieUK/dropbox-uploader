367ce7152cbe3a4d76ee5abf57eab0fd42f9275c
Bradley Allen <valkyrieuk@Labs-MacBook-Air.local>
2013-12-04 15:58:00 +0000

    initial

---
+++ index.php
@@ -0,0 +1,81 @@
+<html>
+    <head>
+         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <title>DropboxUploader</title>
+    </head>
+
+<style>
+h1{}
+body{font-family:arial;font-size:13px;}
+.box{
+background:none repeat scroll 0 0 #F6F6F6;
+border:1px solid #C3C3C3;
+margin-left:100px;
+margin-top:50px;
+width:860px;
+}
+.message{
+background-color:#FFFFE0;
+border:1px solid #E6DB55;
+margin:30px 0 16px 8px;
+padding:12px;
+width:774px;
+}
+</style>
+<body>
+<?php
+if ($_POST) {
+    require 'DropboxUploader.php';
+
+
+    try {
+        // Rename uploaded file to reflect original name
+        if ($_FILES['file']['error'] !== UPLOAD_ERR_OK)
+            throw new Exception('File was not successfully uploaded from your computer.');
+
+        $tmpDir = uniqid('/tmp/DropboxUploader-');
+        if (!mkdir($tmpDir))
+            throw new Exception('Cannot create temporary directory!');
+
+        if ($_FILES['file']['name'] === "")
+            throw new Exception('File name not supplied by the browser.');
+
+        $tmpFile = $tmpDir.'/'.str_replace("/\0", '_', $_FILES['file']['name']);
+        if (!move_uploaded_file($_FILES['file']['tmp_name'], $tmpFile))
+            throw new Exception('Cannot rename uploaded file!');
+
+        // Enter your Dropbox account credentials here
+		$DropboxUploader = new DropboxUploader('USERNAME', 'PASSWORD');
+        $DropboxUploader->upload($tmpFile, $_POST['dest']);
+
+        echo '<span style="color: green;font-weight:bold;margin-left:393px;">File successfully uploaded to my Dropbox!</span>';
+    } catch(Exception $e) {
+        echo '<span style="color: red;font-weight:bold;margin-left:393px;">Error: ' . htmlspecialchars($e->getMessage()) . '</span>';
+    }
+
+    // Clean up
+    if (isset($tmpFile) && file_exists($tmpFile))
+        unlink($tmpFile);
+
+    if (isset($tmpDir) && file_exists($tmpDir))
+        rmdir($tmpDir);
+}
+?>
+<div class="box" align="center">
+		<h1>Dropbox Uploader</h1>
+        <h3>PLTS week video and pics only please</h3>
+        <br>
+		<br>
+		&nbsp;
+		<form method="POST" enctype="multipart/form-data">
+		<input type="file" name="file" multiple /a><br><br>
+		<input type="submit" value="Upload the file to my Dropbox!" />
+		<input style="display:none" type="text" name="dest" value="shared" />
+
+</div>
+
+
+
+
+</body>
+</html>
